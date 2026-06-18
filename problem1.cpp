039583299d6d68095ec8b68785e48719a74b2c3c
pankaj chauhan <pankajjune27@gmail.com>
2026-06-18 22:44:28 +0530

    Add Day 1 Linked List solutions

---
+++ problem1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node* next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+bool hasCycle(Node* head)
+{
+    Node* slow = head;
+    Node* fast = head;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+            return true;
+    }
+
+    return false;
+}
+
+int main()
+{
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+
+    head->next->next->next->next = head->next;
+
+    if (hasCycle(head))
+        cout << "Cycle Found";
+    else
+        cout << "No Cycle";
+
+    return 0;
+}
---
+++ problem3.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node* next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+Node* findMiddle(Node* head)
+{
+    Node* slow = head;
+    Node* fast = head;
+
+    while(fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
+
+int main()
+{
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+
+    Node* middle = findMiddle(head);
+
+    cout << "Middle Node: " << middle->data;
+
+    return 0;
+}
